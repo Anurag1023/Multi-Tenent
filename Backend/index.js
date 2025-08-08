615f8fb6078dfab77456573e7a936c7076f73498
Anurag1023 <rajputanuragsingh10@gmail.com>
2025-08-08 13:26:55 +0530

    axios 3.1

--- Backend/index.js
+++ Backend/index.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  origin: "*",
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: "*"
