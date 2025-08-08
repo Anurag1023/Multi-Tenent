ee8db923cea8a101640bb2d541a1473561c31249
Anurag1023 <rajputanuragsingh10@gmail.com>
2025-08-08 12:12:27 +0530

    updated cors 2.0

--- Backend/server.js
+++ Backend/server.js
@@ -20,12 +20,13 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
-    credentials: true,
-  })
-);
+
+const corsOptions = {
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
+app.use(cors(corsOptions));
 
 
 app.use("/api/auth", authRoutes);
