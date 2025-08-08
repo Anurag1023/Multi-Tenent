80650fca95a82db954b12987ffee74fc5a6c19d7
Anurag1023 <rajputanuragsingh10@gmail.com>
2025-08-08 13:09:14 +0530

    updated axios

--- Backend/index.js
+++ Backend/index.js
@@ -22,9 +22,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: '*',
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: "*"
 };
 app.use(cors(corsOptions));
 
--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://multi-tenent-21w6ozry4-anurag-singhs-projects-39fc9ce5.vercel.app/api" || "http://localhost:5000/api",
+  baseURL: "https://multi-tenent-iifw-git-main-anurag-singhs-projects-39fc9ce5.vercel.app/api" || "http://localhost:5000/api",
   withCredentials: true,
 });
