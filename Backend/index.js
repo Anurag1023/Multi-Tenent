317fc58a15bf4271b0e2c40ef0a5998161605516
Anurag1023 <rajputanuragsingh10@gmail.com>
2025-08-08 13:32:10 +0530

    axios 3.2

--- Backend/index.js
+++ Backend/index.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: "*",
+  origin: ["https://multi-tenent-iifw.vercel.app", "http://localhost:5173"],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: "*"
