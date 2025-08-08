11ceb1bb8256e1acf304640204aa56d79cb836ca
Anurag1023 <rajputanuragsingh10@gmail.com>
2025-08-08 15:59:59 +0530

    try 2.6

--- Backend/index.js
+++ Backend/index.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: ["https://multi-tenent-omega.vercel.app", "http://localhost:5173"],
+  origin: ["https://multi-tenent-iifw.vercel.app", "http://localhost:5173"],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ["Content-Type", "Authorization"], 
