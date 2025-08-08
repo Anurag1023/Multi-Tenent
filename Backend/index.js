425092374cade50fe1dec244e45f5713061a9b52
Anurag1023 <rajputanuragsingh10@gmail.com>
2025-08-08 13:36:39 +0530

    axios 3.3

--- Backend/index.js
+++ Backend/index.js
@@ -25,7 +25,7 @@ const corsOptions = {
   origin: ["https://multi-tenent-iifw.vercel.app", "http://localhost:5173"],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: "*"
+  allowedHeaders: ["Content-Type", "Authorization"], 
 };
 app.use(cors(corsOptions));
 
